2a989cd226956fa54556f7ceba74e71605f73b63
jkulcsar <jhn030564@gmail.com>
2015-09-26 22:29:21 +0000

    Adding yet another directive as-a controller.

--- angular/app.js
+++ angular/app.js
@@ -1,79 +1,75 @@
 (function () {
-		var app = angular.module('gemStore', []);
+	var app = angular.module('gemStore', []);
 
-		app.controller('GalleryController', function () {
-			this.current = 0;
-			this.setCurrent = function (imageNumber) {
-				this.current = imageNumber || 0;
-			};
-		});
+	app.controller('StoreController', function () {
+		this.products = gems;
+	});
 
-		app.controller('StoreController', function () {
-			this.products = gems;
-		});
+	app.controller("ReviewController", function () {
 
-		// app.controller("TabController", function () {
-		// 	this.panel = 1;
+		this.review = {};
 
-		// 	this.isSet = function (checkTab) {
-		// 		return this.panel === checkTab;
-		// 	};
-
-		// 	this.setTab = function (setTab) {
-		// 		this.panel = setTab;
-		// 	};
-		// });
-
-		app.controller("ReviewController", function () {
-
+		this.addReview = function (product) {
+			product.reviews.push(this.review);
 			this.review = {};
+		};
 
-			this.addReview = function (product) {
-				product.reviews.push(this.review);
-				this.review = {};
-			};
+	});
 
-		});
+	app.directive("productDescriptions", function () {
+		return {
+			restrict: 'E',
+			templateUrl: "product-descriptions.html"
+		};
+	});
 
-		app.directive("productDescriptions", function () {
-			return {
-				restrict: 'E',
-				templateUrl: "product-descriptions.html"
-			};
-		});
+	app.directive("productReviews", function () {
+		return {
+			restrict: 'E',
+			templateUrl: "product-reviews.html"
+		};
+	});
 
-		app.directive("productReviews", function () {
-			return {
-				restrict: 'E',
-				templateUrl: "product-reviews.html"
-			};
-		});
+	app.directive("productSpecs", function () {
+		return {
+			restrict: 'A',
+			templateUrl: 'product-specs.html'
+		};
+	});
 
-		app.directive("productSpecs", function () {
-			return {
-				restrict: 'A',
-				templateUrl: 'product-specs.html'
-			};
-		});
+	app.directive("productPanels", function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-panels.html',
+			controller: function () {
+				this.panel = 1;
 
-		app.directive("productPanels", function () {
-				return {
-					restrict: 'E',
-					templateUrl: 'product-panels.html',
-					controller: function () {
-						this.panel = 1;
+				this.isSet = function (checkPanel) {
+					return this.panel === checkPanel;
+				};
 
-						this.isSet = function (checkPanel) {
-							return this.panel === checkPanel;
-						};
+				this.setPanel = function (setPanel) {
+					this.panel = setPanel;
+				};
+			},
+			controllerAs: 'panels'
+		}
+	});
 
-						this.setPanel = function (setPanel) {
-							this.panel = setPanel;
-						};
-					},
-					controllerAs: 'panels'
-				}
-			});
+	app.directive('productGallery', function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'product-gallery.html',
+			controller: function() {
+				this.current = 0;
+				this.setCurrent = function (imageNumber) {
+					this.current = imageNumber || 0;
+				};
+			},
+			controllerAs: 'gallery'
+		};
+	});
+
 
 	var gems = [
 		{
