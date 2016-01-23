80576c6143b09b54bcd47da48464c8d5276d2f77
jkulcsar <jhn030564@gmail.com>
2016-01-23 23:45:20 +0000

    Adding TypeScript introductory code: Angular2 Quick Start and React with TypeScript.

---
+++ ts/todo-typescript-react/js/constants.ts
@@ -0,0 +1,13 @@
+
+// Expose some constants.
+// The constants are used to store the numeric value of the keyboard keys (ENTER_KEY and ESCAPE_KEY) that we will use later to set some events listeners.
+
+// We will also use some values to identify the currently displayed list of tasks by its status.
+
+namespace app.constants {
+  export var ALL_TODOS = 'all';
+  export var ACTIVE_TODOS = 'active';
+  export var COMPLETED_TODOS = 'completed';
+  export var ENTER_KEY = 13;
+  export var ESCAPE_KEY = 27;
+}
