cdd7754f69853dccb00a3d608810903758a3dad1
Janos <janos.kulcsar@gmail.com>
2016-02-19 10:22:41 +0100

    Just started Redux quick intro.

---
+++ redux-intro/04/jsbin.matoka.js
@@ -0,0 +1,102 @@
+
+const counter = (state = 0, action) => {
+  
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+// the store binds together the 3 principles of Redux:
+// const { createStore } = Redux;
+// the above is equivalent to:
+// var createStore = Redux.createStore;
+// or import { createStore } from 'redux';
+
+const { createStore } = Redux;
+
+
+// create the store for the application state,
+// passing in the function to be used as the reducer
+// that manages the state updates
+const store = createStore(counter);
+
+const Counter = ({
+  value,
+  onIncrement,
+  onDecrement
+}) => (
+  <div>
+    <h1>{value}</h1>
+    <button onClick={onIncrement}>+</button>
+    <button onClick={onDecrement}>-</button>
+  </div>
+);
+
+// the store has 3 important functions:
+// 1. get the state of the store / app
+console.log(store.getState());
+
+// 2. dispatch an action to manage the state of the application
+store.dispatch({type: "INCREMENT"});
+console.log(store.getState());
+
+// 3. any time an action has been dispatched, the subscribed function will be called - for example to trigger immediately a rendering
+const render = () => {
+  // manually updating the DOM is definitely not the way...
+  // document.body.innerText = store.getState(); 
+  // using React:
+  ReactDOM.render(
+    <Counter
+      value = {store.getState()},
+      onIncrement = { () => 
+                     store.dispatch( {type: "INCREMENT"})
+                    }
+      onDecrement = { () => 
+                      store.dispatch( {type: "DECREMENT"})
+                    }
+    />,
+    document.getElementById("root")
+  );
+};
+
+store.subscribe(render);
+render();
+
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT'});
+});
+
+
+// simple unit tests to prove that the reducer
+// complies with the three principles
+expect(
+  counter(0, {type: "INCREMENT"})
+).toEqual(1);
+
+expect(
+  counter(1, {type: "INCREMENT"})
+).toEqual(2);
+
+expect(
+  counter(2, {type: "DECREMENT"})
+).toEqual(1);
+
+expect(
+  counter(1, {type: "DECREMENT"})
+).toEqual(0);
+
+expect(
+  counter(2, {type: "SOMETHING_ELSE"})
+).toEqual(2);
+
+expect(
+  counter(undefined, {})
+).toEqual(0);
+
+console.log("Tests passed!");
